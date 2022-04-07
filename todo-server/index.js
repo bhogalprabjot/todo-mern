81bf413297139d3a846aa045df67439bb2e7e55d
bhogalprabjot <bhogalprabjot1@gmail.com>
2022-04-07 23:08:19 +0530

    Stable version 1; needs some bug fixes

--- todo-server/index.js
+++ todo-server/index.js
@@ -2,27 +2,34 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import listRouter from './routes/todo.js';
 import userRouter from './routes/user.js';
 
-
 // initialize express app
 const app = express();
+
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.get('/', (req, res) => {
+    res.send("Welcome to To Do Backend!");
+})
+
 // add routes
-app.use('/list',listRouter);
+app.use('/list', listRouter);
 app.use('/user', userRouter);
-var CONNECTION_URL = 'mongodb://127.0.0.1:27017/todo';
+// var CONNECTION_URL = 'mongodb://127.0.0.1:27017/todo';
 
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((err) => console.log(err.message));
 
